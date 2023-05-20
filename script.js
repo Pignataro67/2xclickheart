de18dc6e4fb7a8b118fd0409997f292492be1100
Pignataro67 <Pignataro67@gmail.com>
2023-05-20 12:35:47 -0700

    spacing

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ const panel = document.querySelector('#panel')
 let selectedRating = 'Satisfied'
 
 ratingsContainer.addEventListener('click', (e) => {
-    if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.nextElementSibling.innerHTML
-    } else if(
-        e.target.parentNode.classList.contains('rating') &&
-        e.target.previousSibling &&
-        e.target.previousElementSibling.nodeName === 'IMG'
-    ) 
-    {
-        removeActive()
-        e.target.parentNode.classList.add('active')
-        selectedRating = e.target.innerHTML
-    }
+  if(e.target.parentNode.classList.contains('rating') && e.target.nextElementSibling) {
+    removeActive()
+    e.target.parentNode.classList.add('active')
+    selectedRating = e.target.nextElementSibling.innerHTML
+  } else if(
+    e.target.parentNode.classList.contains('rating') &&
+    e.target.previousSibling &&
+    e.target.previousElementSibling.nodeName === 'IMG'
+  ) 
+  {
+    removeActive()
+    e.target.parentNode.classList.add('active')
+    selectedRating = e.target.innerHTML
+  }
 })
