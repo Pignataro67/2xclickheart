a8be31076b97058aa7a5f5fa2d73361e8a51ef1e
Pignataro67 <Pignataro67@gmail.com>
2023-08-29 05:57:26 -0700

    CSS

--- script.js
+++ script.js
@@ -11,3 +11,11 @@ slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
+
+
+const changeSlide = (direction) => {
+  const sliderHeight = sliderContainer.clientHeight
+  
+  slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`
+  slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`
+}
